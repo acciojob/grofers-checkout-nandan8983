7ad71394579465e4d3e086ae187878d0547bcd11
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2025-06-14 22:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,33 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const oldTotal = document.getElementById("total-row");
+  if (oldTotal) oldTotal.remove();
+
+  const priceCells = document.querySelectorAll(".price");
+  let total = 0;
+
+  priceCells.forEach(cell => {
+    total += Number(cell.textContent);
+  });
+
+
+  const table = document.querySelector("table");
+  const totalRow = document.createElement("tr");
+  totalRow.id = "total-row"; 
+
+  const totalCellLabel = document.createElement("td");
+  totalCellLabel.textContent = "Total";
+  totalCellLabel.style.fontWeight = "bold";
+
+  const totalCellValue = document.createElement("td");
+  totalCellValue.textContent = total;
+  totalCellValue.style.fontWeight = "bold";
+
+  totalRow.appendChild(totalCellLabel);
+  totalRow.appendChild(totalCellValue);
+
+  table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
